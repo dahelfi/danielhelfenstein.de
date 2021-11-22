e8271793869fa13d660c9f101b95a9464d57b442
Daniel Helfenstein <d.helfenstein@mail.de>
2021-11-22 21:36:21 +0100

    start to work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'danielhelfenstein';
+export class AppComponent implements OnInit {
+  images = ['laptop-g7677161ed_1920.jpg', 'technology-ga2bc0d0da_1920.jpg', 'oxford-gbff85860c_1920.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage(){
+    setInterval(() =>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() =>{
+        this.showImage = true;
+      }, 10);
+    }, 5000);
+  }
 }
